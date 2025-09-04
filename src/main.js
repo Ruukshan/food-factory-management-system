a1ce6edfad95bea46dd14aabea6c55de1c78eb8d
Ruukshan <rukshaninduwara123@gmail.com>
2025-09-04 23:01:20 +0530

    Initialize vuetify and MDI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+
+import '@mdi/font/css/materialdesignicons.css';
 
 const vuetify = createVuetify({
   components,
