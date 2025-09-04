b1120110af7ae0c2c9d6eb012b25fcbe41b6e832
Ruukshan <rukshaninduwara123@gmail.com>
2025-09-04 23:27:37 +0530

    Update App.vue to manage product data

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-card-title>Food Factory Management System - Production</v-card-title>
+        <v-card-text>
+          <FormComponent @add-product="addProduct" />
+          <TableComponent :products="products" @delete-product="deleteProduct" />
+          <v-btn color="primary" block class="mt-4" @click="submit">Submit</v-btn>
+        </v-card-text>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
+<script setup>
+ import { ref } from 'vue';
+ import FormComponent from './components/FormComponent.vue';
+ import TableComponent from './components/TableComponent.vue';
+
+ const products = ref([]);
+ const addProduct = (product) => {
+   products.value.push(product);
+ };
+ const deleteProduct = (index) => {
+   products.value.splice(index, 1);
+ };
+ const submit = () => {
+   console.log('Submitted data: ', products.value);
+   alert('Data submitted successfully!');
+ }
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
