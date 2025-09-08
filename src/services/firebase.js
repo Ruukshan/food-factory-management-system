5e9e90a60c4f82ffcac5817e44d06bbf0becc8ea
osandaDev <osanda1208@gmail.com>
2025-09-08 16:46:19 +0530

    Remove Comment

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,7 +4,7 @@ import { Firestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
